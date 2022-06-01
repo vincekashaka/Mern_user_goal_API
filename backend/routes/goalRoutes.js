ed3ab00bf374f0a5ec8709d2c39c4c349b9f6aea
Vincent Kashaka <vincekashaka@gmail.com>
2022-06-01 23:12:12 +0200

    added a delete function

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router  = express.Router()
-const {getGoals, setGoals, updateGoals,deleteGoals} = require('../controller/goalController')
+const {
+    getGoals, 
+    setGoals, 
+    updateGoals,
+    deleteGoals
+} = require('../controller/goalController')
 
-router.route('/').get(getGoals).post(setGoals)
+//Protect goals
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/:id').put(updateGoals).delete(deleteGoals)
+router.route('/').get(protect, getGoals).post(protect, setGoals)
+
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals)
 
 
 // router.get('/', getGoals)
