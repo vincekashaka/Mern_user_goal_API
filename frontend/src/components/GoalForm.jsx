3698cb66102533392ebc8090b2d04eae32a3d37c
Vincent Kashaka <vincekashaka@gmail.com>
2022-06-06 21:22:08 +0200

    added dashboard functionality

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,9 +1,36 @@
 import {useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useDispatch} from 'react-redux'
+import { createGoal } from '../features/goals/goalSlice'
 
 const GoalForm = () => {
+
+  const [text, setText] = useState('')
+
+  const dispatch = useDispatch()
+
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(createGoal({text}))
+    setText('')
+  }
+
   return (
-    <div>GoalForm</div>
+    <section className='form'>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="text">Goal</label>
+          <input type="text" name = 'text' id='text'value=
+          {text} onChange={(e) => setText(e.target.value)} />
+        </div>
+          <div className="form-group">
+            <button className="btn btn-block">
+              Add Goal
+            </button>
+          </div>
+      </form>
+    </section>
   )
 }
 
